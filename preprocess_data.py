9f9c514912c0fa128af55857d0c59ab7d14b61bd
Harry Durnberger <harrydurnberger@gmail.com>
2023-03-06 17:11:11 +0000

    sort categories

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,13 +1,10 @@
-#%%
 import os
 from pathlib import Path
 from PIL import Image
 from PIL import UnidentifiedImageError
 import multiprocessing as mp
 import pandas as pd
-
-path_to_data = Path(__file__).parent / f"./machine-learning-in-science-ii-2023"
-training_norm = pd.read_csv(path_to_data/'training_norm.csv')
+import shutil
 
 def image_generator(directory):
     for filename in os.listdir(directory):
@@ -65,29 +62,31 @@ def filter_non_integer_speeds(directory):
 def filter_images(directory):
     filter_corrupted_images(directory)
     filter_non_integer_speeds(directory)
-    
-filter_images(path_to_data/'training_data/training_data')
-#%%
+
 def create_category_csvs(group):
     speed = group['speed'].iloc[0]
     file = f"training_norm_{speed}.csv"
     group.to_csv(os.path.join(path_to_data, file), index=False)
 
-training_norm.groupby(['speed']).apply(create_category_csvs)
-speed_zero = pd.read_csv(path_to_data/'training_norm_0.0.csv')
-speed_one = pd.read_csv(path_to_data/'training_norm_1.0.csv')
-#%%
-import shutil
-
-source_path = path_to_data/'training_data/training_data'
-destination_path = path_to_zeros
-
-for filename in os.listdir(source_path):
-    if filename.endswith('.png'):
-        # construct the paths for the source and destination files
+def move_images(images, destination_path):
+    source_path = path_to_data/'training_data/training_data'
+    for image_num in images:
+        filename = f"{image_num}.png"
         source_file_path = os.path.join(source_path, filename)
         destination_file_path = os.path.join(destination_path, filename)
-        
-        # move the file to the destination folder
         shutil.move(source_file_path, destination_file_path)
-#%%
+        
+def categorise_images():
+    training_norm.groupby(['speed']).apply(create_category_csvs)
+    speed_zero = pd.read_csv(path_to_data/'training_norm_0.0.csv')
+    speed_one = pd.read_csv(path_to_data/'training_norm_1.0.csv')
+
+    images_speed_zero = speed_zero['image_id'].tolist()
+    images_speed_one = speed_one['image_id'].tolist()
+    move_images(images_speed_zero, path_to_data/'training_data/0')
+    move_images(images_speed_one, path_to_data/'training_data/1')
+
+path_to_data = Path(__file__).parent / f"./machine-learning-in-science-ii-2023"
+training_norm = pd.read_csv(path_to_data/'training_norm.csv')
+filter_images(path_to_data/'training_data/training_data')
+categorise_images()
