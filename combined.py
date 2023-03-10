0f725c965a6ccaaf47b3a48aba44f64955c02b49
Harry Durnberger <harrydurnberger@gmail.com>
2023-03-10 14:03:29 +0000

    corrections

--- combined.py
+++ combined.py
@@ -21,6 +21,7 @@ if num_physical_devices > 0:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 batch_size = 20
+epochs = 6
 mobilenet_v2 = True
 inception_v3 = False
 
@@ -30,11 +31,11 @@ if mobilenet_v2:
     image_shape = (224, 224)
 elif inception_v3:
     inception_v3 = "https://tfhub.dev/google/imagenet/inception_v3/classification/5"
-    image_shape = (299,299)
+    image_shape = (299, 299)
     classifier_model = inception_v3
 
 if on_colab:
-    path_to_data = Path("/content/drive/My Drive/machine-learning-in-science-ii-2023")
+    path_to_data = Path("/content/drive/MyDrive/machine-learning-in-science-ii-2023")
     image_paths_csv = pd.read_csv(str(path_to_data/'training_norm_paths_googledrive.csv'))
 else:
     path_to_data = Path(__file__).parent / f"./machine-learning-in-science-ii-2023"
@@ -47,17 +48,20 @@ dataset = tf.data.Dataset.from_tensor_slices((image_paths_csv['image_path'],
 
 # Define a function that maps each row to an image and a pair of labels
 def load_image_and_labels(image_path, speed_label, angle_label):
-  image = tf.io.read_file(image_path)
-  image = tf.image.decode_png(image, channels=3)
-  image = tf.image.resize(image, image_shape)
-  # Convert the class label to an integer
-  speed_label = tf.cast(speed_label, tf.int32)
-  # Convert the regression label to a float
-  angle_label = tf.cast(angle_label, tf.float32)
-  return image, (speed_label, angle_label)
+    image = tf.io.read_file(image_path)
+    image = tf.image.decode_png(image, channels=3)
+    image = tf.image.resize(image, image_shape)
+    # Convert the class label to an integer
+    speed_label = tf.cast(speed_label, tf.int32)
+    # Convert the regression label to a float
+    angle_label = tf.cast(angle_label, tf.float32)
+    #angle_label = tf.cast(angle_label * 16, tf.int32) # multiply by 16 to get values between 0 and 16
+    #angle_label = tf.one_hot(angle_label, depth=17)
+    return image, (speed_label, angle_label)
 
 dataset = dataset.map(load_image_and_labels).batch(batch_size) # Apply the function to each batch of data
 num_batches = dataset.cardinality().numpy() # Calculate the number of batches in the dataset
+
 # Shuffle the data using a buffer size equal to or larger than the number of elements in the dataset
 dataset = dataset.shuffle(buffer_size=num_batches*batch_size)
 
@@ -72,25 +76,6 @@ val_set = dataset.skip(train_batches)
 AUTOTUNE = tf.data.AUTOTUNE
 train_set = train_set.cache().prefetch(buffer_size=AUTOTUNE)
 val_set = val_set.cache().prefetch(buffer_size=AUTOTUNE)
-
-classifier = tf.keras.Sequential([
-    hub.KerasLayer(classifier_model, input_shape=image_shape+(3,))
-])
-classifier.trainable = False
-
-inp = tf.keras.layers.Input((image_shape[0], image_shape[1], 3)) #RGB images of size (x, y)
-
-#add the classifier layers before the dense layers
-x = classifier(inp) #assuming classifier is a layer or a model
-
-#you can define multiple outputs from x
-classification_output = tf.keras.layers.Dense(1, activation=None, name='speed')(x) #binary classification
-regression_output = tf.keras.layers.Dense(1, activation='linear', name='angle')(x) #regression
-
-#create a model with one input and two outputs
-model = tf.keras.models.Model(inputs=inp, outputs=[classification_output, regression_output])
-
-model.summary()
 
 def f1_score(y_true, y_pred):
     """
@@ -119,13 +104,35 @@ def f1_score(y_true, y_pred):
     f1_score = 2 * ((precision * recall) / (precision + recall + K.epsilon()))
     return f1_score
 
-#training the model
+classifier = tf.keras.Sequential([
+    hub.KerasLayer(classifier_model, input_shape=image_shape+(3,))
+])
+classifier.trainable = False
+
+inp = tf.keras.layers.Input((image_shape[0], image_shape[1], 3)) #RGB images of size (x, y)
+
+#add the classifier layers before the dense layers
+x = classifier(inp) #assuming classifier is a layer or a model
+
+#you can define multiple outputs from x
+classification_output = tf.keras.layers.Dense(1, activation=None, name='speed')(x) #binary classification
+regression_output = tf.keras.layers.Dense(1, activation='linear', name='angle')(x) #regression
+
+#create a model with one input and two outputs
+model = tf.keras.models.Model(inputs=inp, outputs=[classification_output, 
+                                                   regression_output
+                                                   ])
+
+model.summary()
+
+#train the model
 
 model.compile(
     optimizer = tf.keras.mixed_precision.LossScaleOptimizer(tf.keras.optimizers.Adam()),
     loss={
         'speed': tf.keras.losses.BinaryCrossentropy(from_logits=True),
         'angle': tf.keras.losses.MeanSquaredError()
+        #'angle': tf.keras.losses.CategoricalCrossentropy(from_logits=False)
     },
     metrics={
         'speed': ['accuracy', f1_score],
@@ -137,8 +144,6 @@ model.compile(
     }
 )
 
-EPOCHS = 6
-
 history = model.fit(train_set,
-                   epochs=EPOCHS,
+                   epochs=epochs,
                    validation_data=val_set)
